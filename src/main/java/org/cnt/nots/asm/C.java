d3028ab20483c562660ad04b1ca0e1511cff747b
coding-new-talking <1511937685@qq.com>
2019-08-06 14:12:44 +0800

    update

--- src/main/java/org/cnt/nots/asm/C.java
+++ src/main/java/org/cnt/nots/asm/C.java
@@ -1,9 +1,12 @@
 package org.cnt.nots.asm;
 
+import org.cnt.nots.asm.ann.Ann1;
+
 /**
  * @author lixinjie
  * @since 2019-07-18
  */
+@Ann1(name = "C.Ann1")
 public class C<T, V> implements Z {
 
 	public int getInt(int i) {
--- src/main/java/org/cnt/nots/asm/ann/Ann1.java
+++ src/main/java/org/cnt/nots/asm/ann/Ann1.java
@@ -4,12 +4,14 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Documented
 @Retention(RUNTIME)
 @Target(TYPE)
+@Inherited
 /**
  * @author lixinjie
  * @since 2019-07-24
